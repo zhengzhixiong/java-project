06e3707c3517eb3baab941e28c3c8df96c56b4ca
max <358744287@qq.com>
2022-11-04 11:50:38 +0800

    支持springboot 3.0.0 RC 编译除本地镜像方式

--- native-demo/src/main/java/com/test/Controller.java
+++ native-demo/src/main/java/com/test/Controller.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@GetMapping("/")
-	public String index() {
-		return "Native Demo For Spring Boot!";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "Native Demo For Spring Boot!";
+    }
 
 }
